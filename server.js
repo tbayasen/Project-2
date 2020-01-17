c44ccc71f6662904f0ae6070869a08b099e868d1
tyronebayasen99 <tyronejbayasen@gmail.com>
2020-01-16 18:27:21 -0800

    set up server js

---
+++ server.js
@@ -0,0 +1,48 @@
+// Require Dependencies
+require('dotenv').config();
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var db = require('./app/models');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Handlebars
+app.engine(
+	'handlebars',
+	exphbs({
+		defaultLayout: 'main'
+	})
+);
+app.set('view engine', 'handlebars');
+
+// Routes
+require('./app/routes/apiRoutes')(app);
+require('./app/routes/htmlRoutes')(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === 'test') {
+	syncOptions.force = true;
+}
+
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+	app.listen(PORT, function() {
+		console.log(
+			'==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+			PORT,
+			PORT
+		);
+	});
+});
+
+module.exports = app;
