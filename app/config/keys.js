1b5c6f4a03d110e1b21cde6cb84ec528a6a55e41
CaoKao <bksaechao@gmail.com>
2020-01-23 20:44:42 -0800

    grabbing frontend stuff

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,4 +1,4 @@
-exports.goodreads = {
+module.exports.goodreads = {
 	key: process.env.GOODREADS_KEY,
 	secret: process.env.GOODREADS_SECRET
 };
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
-const keys = require('./app/models/keys.js');
-const Goodreads = require('goodreads-api-node');
-const gr = new Goodreads(keys.goodreads);
+const express = require('express');
 
-gr.getBooksByAuthor('175417')
-	.then(console.log);
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require('./app/models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+require('./app/routes/apiRoutes')(app);
+
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('App listening on PORT ' + PORT);
+	});
+});
